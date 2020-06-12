af3118b66f9e72a39e0920f91bf0971d138d92f5
Nicholas Liang <z5215234@student.unsw.edu.au>
2020-06-13 02:03:19 +1000

    New Base Template with CSS sheet static folder added

--- website/website/settings.py
+++ website/website/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
