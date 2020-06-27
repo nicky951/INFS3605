89b465925d2d784cfe54ff99cfacae49310b0d8b
Nicholas Liang <z5215234@student.unsw.edu.au>
2020-06-27 15:01:19 +1000

    user login page [Not Complete]

--- website/webapp/urls.py
+++ website/webapp/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('nextpage', views.index, name='index'),
-    path('', views.testhtml, name='testhtml')
+
 ]
--- website/website/settings.py
+++ website/website/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_REDIRECT_URL = '/'
