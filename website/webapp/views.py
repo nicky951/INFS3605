aa85daee4c97719b3a9f2d508416ee5f42f46ea8
Nicholas Liang <z5215234@student.unsw.edu.au>
2020-07-01 18:04:47 +1000

    Updated Photos Mikayla and Charlotte

--- website/webapp/views.py
+++ website/webapp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+#import requests
+
 # Create your views here.
+# UG6f30PnzgFUgFKQ1lu1H1HvGMS0GOta
+# https://api.nytimes.com/svc/search/v2/articlesearch.json?q=computer%20security&page=0&api-key=UG6f30PnzgFUgFKQ1lu1H1HvGMS0GOta
+
+#def home(requests):
+#    response = requests.get('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=computer%20security&page=0&api-key=UG6f30PnzgFUgFKQ1lu1H1HvGMS0GOta')
+#    cyberdata = response.json()
